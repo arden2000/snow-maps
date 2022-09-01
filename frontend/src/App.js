cd46d91dbc958dfaf273cadf7bc72910961efe90
Arden Ozdere <ardenozdere@gmail.com>
2022-09-01 12:22:31 -0700

    undid aws-exports change

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,7 @@ import { Amplify, Auth } from 'aws-amplify';
 import Home from './components/Home.jsx';
 import SignIn from './components/SignIn.jsx';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import awsconfig from '../aws-exports';
+import awsconfig from './aws-exports';
 import 'mapbox-gl/dist/mapbox-gl.css';
 import Main from './components/Main.jsx'
 
